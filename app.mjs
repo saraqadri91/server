a795300469d74e2ffe3fa9f4018f14a44e07a181
saraqadri91 <qadrisara91@gmail.com>
2025-02-19 10:51:31 +0500

    comp

--- app.mjs
+++ app.mjs
@@ -1,16 +1,35 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 9807;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use("/", express.static(path.join(__dirname, "frontend/dist")));
 
 app.get("/", (req, res) => {
-  res.send("This is  port ");
+  res.send("GET request running");
 });
-app.get("/sara", (req, res) => {
-  res.send("This is sara port ");
+
+app.get("/weather", (req, res) => {
+  res.json({ user: "tobi" });
 });
-app.get("/ayesha", (req, res) => {
-  res.send("This is ayesha port ");
+
+app.get("/weather/:cityName", (req, res) => {
+  res.json(req.params);
+});
+
+app.get("/weather/:cityName/:side", (req, res) => {
+  res.json(req.params);
+});
+
+// Middleware to handle unmatched routes
+app.use((req, res) => {
+  res.send("Middleware running - Route not found");
 });
 
 app.listen(port, () => {
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+import "./App.css";
+import LoginForm from './Component/Todo';
+
+
+const App = () => {
+  return (
+
+    <div> 
+      <h1>frontend</h1>
+    </div>
+  );
+};
+
+export default App;
